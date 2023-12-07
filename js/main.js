09087a9534f6540b150a96688d24abd89311808b
Time For <timeformistake@gmail.com>
2023-12-07 21:54:35 +0200

    Upd

--- js/main.js
+++ js/main.js
@@ -1,75 +1,61 @@
 const API_URL = "https://jsonplaceholder.typicode.com";  
 
-window.onload = initializeUserProfile;
+window.onload = loadUsersData;
 
-async function initializeUserProfile() {
-  const users = await loadUsers();
-  displayUserProfile(users);
+async function loadUsersData() {
+    const responseUsers = await fetch(`${API_URL}/users`);
+    const responsePosts = await fetch(`${API_URL}/posts`);
+    const responsePhotos = await fetch(`${API_URL}/photos`);
+    
+    const users = await responseUsers.json();
+    const posts = await responsePosts.json();
+    const photos = await responsePhotos.json();
+
+    renderUsersProfile(users, posts, photos);
 }
 
-async function loadUsers() {
-  const response = await fetch(`${API_URL}/users`);
-  return response.json();
+
+function renderUsersProfile(users, posts, photos) {
+    const profileContainer = document.querySelector('.profile-container');
+
+    users.forEach(user => {
+
+        const postsId = posts.filter(post => post.userId === user.id);
+        const firstPost = postsId[0].body;
+
+        const photosId = photos.filter(photo => photo.albumId === user.id);
+
+        const profileBlock = document.createElement('div');
+        const userPhoto = document.createElement('img');
+        const userInfo = document.createElement('div');
+        const userPost = document.createElement('div')
+        const postButton = document.createElement('button');
+      
+        profileBlock.classList.add('profile-block');
+        userPhoto.classList.add('user-photo');
+        postButton.classList.add('post-button');
+      
+        userInfo.innerHTML = `
+          <strong>${user.username}</strong>
+          <p>${user.name}</p>
+          <p>${user.email}</p>
+        `;
+        userPost.innerHTML = firstPost;
+        userPhoto.src = photosId[0].url;
+        postButton.textContent = `View More`;
+      
+        postButton.addEventListener('click', () => {
+          openNewWindowWithPosts(user.id);
+        });
+      
+        profileBlock.append(userPhoto, userInfo, userPost, postButton);
+        profileContainer.appendChild(profileBlock);
+      })
 }
 
-async function displayUserProfile(users) {
-  const profileContainer = document.querySelector('.profile-container');
-  
-  for (const user of users) {
-    const userProfile = await createUserProfile(user);
-    profileContainer.appendChild(userProfile);
-  }
+function openNewWindowWithPosts(userId) {
+    window.location.href = `user-posts.html#${userId}`;
 }
-
-async function createUserProfile(user) {
-  const profileBlock = document.createElement('div');
-  const userPhoto = document.createElement('img');
-  const userNickname = document.createElement('p');
-  const userName = document.createElement('p');
-  const userEmail = document.createElement('p');
-  const userPost = document.createElement('p')
-  const postButton = document.createElement('button');
-
-  
-  profileBlock.classList.add('profile-block');
-  userPhoto.classList.add('user-photo');
-  postButton.classList.add('post-button');
-
-  userNickname.innerHTML = user.username;
-  userName.innerHTML = user.name;
-  userEmail.innerHTML = user.email;
-  postButton.innerHTML = `View More`;
-
-  const post = await loadPostForUser(user.id);
-  userPost.innerHTML = post.body;
-
-  const photo = await loadPhotoForUser(user.id);
-  userPhoto.src = photo[0].url;
-
-  postButton.addEventListener('click', () => {
-    openNewWindowWithPosts(user.id);
-  });
-
-  profileBlock.append(userPhoto, userNickname, userName, userEmail, userPost, postButton);
-  return profileBlock;
-}
-
-async function loadPhotoForUser(id) {
-  const response = await fetch(`${API_URL}/photos?id=${id}`);
-  const photos = response.json();
-  return photos;
-}
-
-async function loadPostForUser(userId) {
-  const response = await fetch(`${API_URL}/posts?userId=${userId}`);
-  const posts = await response.json();
-  return posts[0];
-} 
-
-async function openNewWindowWithPosts(userId) {
-  window.location.href = `user-posts.html#${userId}`
-}
-
 
 
 
--- js/user-posts.js
+++ js/user-posts.js
@@ -1,14 +1,10 @@
 const API_URL = "https://jsonplaceholder.typicode.com";
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Отримати ідентифікатор користувача з анкора в URL 
-    // Код, який має виконатися після завантаження DOM
     const userId = window.location.hash.substring(1);
     loadAndDisplayUserPosts(userId);
 
-    
     document.getElementById('backButton').addEventListener('click', () => {
-        // Повернення на головну сторінку
         window.location.href = 'index.html';
     });
 });
@@ -16,16 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
 async function loadAndDisplayUserPosts(userId) {
     const mainContainer = document.getElementById('main-container');
     const userPosts = document.getElementById('user-posts');
+    const gallery = document.getElementById('gallery');
 
     const user = await loadUser(userId);
+    const posts = await loadPostsForUser(userId);
+    const photos = await loadPhotosForUser(userId);
 
     const userInfo = document.createElement('div');
+    userInfo.classList.add('user-info');
     userInfo.innerHTML = `<p>Username: ${user.username}</p><p>Name: ${user.name}</p><p>Email: ${user.email}</p>`;
+    userPosts.innerHTML = posts.map(post => `<strong>${post.title}</strong><p>${post.body}</p>`).join('');
+    photos.forEach(photo => {
+        const userPhotos = document.createElement('img');
+        userPhotos.classList.add('user-photos');
+        userPhotos.src = photo.url;
+        gallery.appendChild(userPhotos);
+    })
 
-    const posts = await loadPostsForUser(userId);
-    userPosts.innerHTML = posts.map(post => `<p><strong>${post.title}</strong></p><p>${post.body}</p>`).join('');
-    
-    mainContainer.insertBefore(userInfo, userPosts);
+    mainContainer.insertBefore(userInfo, userPosts, gallery);
 }
 
 async function loadUser(userId) {
@@ -38,4 +42,10 @@ async function loadPostsForUser(userId) {
     const response = await fetch(`${API_URL}/posts?userId=${userId}`);
     const posts = await response.json();
     return posts;
+}
+
+async function loadPhotosForUser(userId) {
+    const response = await fetch(`${API_URL}/photos?albumId=${userId}`);
+    const photos = await response.json();
+    return photos;
 }
